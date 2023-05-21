1274b2fae6e2a8a54258b1277d7f42e04ca5b928
JRH89 <thegemlins@gmail.com>
2023-05-21 00:38:02 -0700

    ok

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,31 @@ import classes from './footer.module.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className={classes.footer}>
+      <div className={classes.wrapper}>
+        <div className={classes.col}>
+          <h2>About the App</h2>
+          <p>
+            Lorem ipsum and sobar sit ameit frah neifar
+            adjfajjislkj! Lorem ipsum and sobar sit ameit frah neifar
+            adjfajjislkj! Lorem ipsum and sobar sit ameit fra
+          </p>
+        </div>
+        <div className={classes.col}>
+          <h2>Contact</h2>
+          <span>Phone: 555 555 5555</span>
+          <span>Email: gamedevjared@gmail.com</span>
+          <span>Github: JRH89</span>
+        </div>
+        <div className={classes.col}>
+          <h2>Location</h2>
+          <span>Continent: N. America</span>
+          <span>Country: U.S.A</span>
+          <span>Current Location: Los Angeles</span>
+        </div>
+      </div>
+    </footer>
+    
   )
 }
 
