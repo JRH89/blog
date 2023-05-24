0792ed2a0c0c69a190895401b30152ad6901e519
JRH89 <thegemlins@gmail.com>
2023-05-24 05:05:14 -0700

    ok

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -5,13 +5,20 @@ import classes from './blog.module.css'
 import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 import {BsFillPencilFill} from 'react-icons/bs'
-import { AiFillDelete } from 'react-icons/ai'
+import {
+	AiFillDelete,
+	AiFillLike,
+	AiOutlineLike
+} from 'react-icons/ai'
+import { format } from 'timeago.js'
+import { useRouter } from 'next/navigation'
 
 const BlogDetails = () => {
 	const [blogDetails, setBlogDetails] = useState("")
 	const { data: session } = useSession()
 	const [blogLikes, setBlogLikes] = useState(0)
 	const [isLiked, setIsliked] = useState(false)
+	const router = useRouter()
 	
 	useEffect(() => {
 		async function fetchBlog() {
@@ -19,14 +26,24 @@ const BlogDetails = () => {
 			const blog = await res.json()
 
 			setBlogDetails(blog)
-			setIsliked(blog.likes.includes(session?.user?._id))
-			setBlogLikes(blog.likes.length)
+			setIsliked(blog?.likes?.includes(session?.user?._id))
+			setBlogLikes(blog?.likes?.length || 0)
 			
 		}
 		session && fetchBlog()
 	}, [session])
 
 	const handleDelete = async () => {
+		try {
+			const confirmModal = confirm("Are you sure you want to delete your blog?")
+			
+		
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	const handleLike = async() => {
 		
 	}
 
@@ -39,7 +56,7 @@ const BlogDetails = () => {
 					{blogDetails?.authorId?._id.toString() === session?.user?._id.toString()
 						? (
 							<div className={classes.controls}>
-								<Link className={classes.editButton} to={`/blog/edit/${ctx.params.id}`}>
+								<Link className={classes.editButton} href={`/blog/edit/${ctx.params.id}`}>
 									Edit <BsFillPencilFill />
 								</Link>
 								<button onClick={handleDelete} className={classes.deleteButton}>
@@ -49,11 +66,30 @@ const BlogDetails = () => {
 							</div>
 						)
 						: (
-							
+							<div className={classes.author}>
+								Author: <span>{blogDetails?.authorId?.username}</span>
+							</div>
 						)
 					}
 			</div>
+			<div className={classes.row}>
+				<div className={classes.category}>
+					Category:
+					<span>{blogDetails?.category}</span>
+				</div>
+				<div className={classes.right}>
+					{blogLikes} {" "} {isLiked ? <AiFillLike size={20} onClick={handleLike}/> : <AiOutlineLike size={20} onClick={handleLike}/>}
+				</div>
+			</div>
+				<div className={classes.row}>
+					<p>{blogDetails?.desc}</p>
+					<span>Posted: <span>{format(blogDetails?.createdAt)}</span></span>
+				</div>
+				<div className={classes.commentSection}>
+					<div className={classes.commentInput}></div>
+				</div>
 		</div>
+		
 	</div>
   	)
 }
