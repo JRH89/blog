1fd07a6d5839d72415e3135c302fdc77c7d9cb37
JRH89 <thegemlins@gmail.com>
2024-06-03 23:22:35 -0700

    firebase and recaptcha for emails

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,32 +1,71 @@
+// Footer.js
 "use client"
-import React from "react"
-import { useForm } from "react-hook-form"
+import React, { useState } from "react"
 import { GithubIcon, LinkedinIcon, TwitterIcon } from "../Icons"
 import Link from "next/link"
 import siteMetadata from "@/src/utils/siteMetaData"
+import { db } from "@/firebase"
+import { collection, addDoc, Timestamp } from "firebase/firestore"
+import ReCAPTCHA from "react-google-recaptcha" // Import ReCAPTCHA
 
 const Footer = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm()
-  const onSubmit = (data) => console.log(data)
-  console.log(errors)
+  const [email, setEmail] = useState("")
+  const [error, setError] = useState(null)
+  const [success, setSuccess] = useState(false)
+  const [captchaValue, setCaptchaValue] = useState(null)
+
+  const handleInputChange = (e) => {
+    setEmail(e.target.value)
+  }
+
+  const handleCaptchaChange = (value) => {
+    setCaptchaValue(value)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setError(null) // Reset error state
+    setSuccess(false) // Reset success state
+
+    if (!email) {
+      setError("Email is required.")
+      return
+    }
+
+    if (!captchaValue) {
+      setError("Please complete the reCAPTCHA.")
+      return
+    }
+
+    try {
+      // Add the email to the "blogEmails" collection in Firestore
+      const docRef = await addDoc(collection(db, "blogEmails"), {
+        email,
+        timestamp: Timestamp.fromDate(new Date()), // Use Timestamp to store the current date and time
+      })
+
+      setSuccess(true)
+      setEmail("")
+      setCaptchaValue(null) // Reset captcha
+    } catch (error) {
+      setError("Error adding email. Please try again.")
+    }
+  }
 
   return (
     <div className="mb-[96px]">
-      <footer className="mt-16  rounded-2xl bg-dark dark:bg-accentDark/90 m-2 sm:m-10 flex flex-col items-center text-light dark:text-dark">
+      <footer className="mt-16 rounded-2xl bg-dark dark:bg-accentDark/90 m-2 sm:m-10 flex flex-col items-center text-light dark:text-dark">
         <h2 className="mt-6 text-lg">Join my mailing list...</h2>
         <form
-          onSubmit={handleSubmit(onSubmit)}
+          onSubmit={handleSubmit}
           className="mt-2 w-fit sm:min-w-[384px] flex items-stretch bg-light dark:bg-dark p-1 sm:p-2 rounded mx04"
         >
           <input
             type="email"
             placeholder="Enter your email"
-            {...register("email", { required: true, maxLength: 80 })}
-            className="w-full bg-transparent pl-2 sm:pl-0 text-dark focus:border-dark focus:ring-0 border-0 border-b mr-2 pb-1"
+            value={email}
+            onChange={handleInputChange}
+            className="w-full bg-transparent pl-2 sm:pl-0 text-dark dark:text-light focus:border-dark focus:ring-0 border-0 border-b mr-2 pb-1"
           />
 
           <input
@@ -34,6 +73,13 @@ const Footer = () => {
             className="bg-dark text-light dark:text-dark dark:bg-light cursor-pointer font-medium rounded px-3 sm:px-5 py-1"
           />
         </form>
+        <ReCAPTCHA
+          sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY} // Use your reCAPTCHA site key
+          onChange={handleCaptchaChange}
+          className="mt-2 mb-2"
+        />
+        {error && <p className="text-red-500">{error}</p>}
+        {success && <p className="text-white font-bold">Email submitted successfully!</p>}
         <div className="flex items-center mt-8">
           <a
             href={siteMetadata.linkedin}
@@ -62,19 +108,9 @@ const Footer = () => {
           >
             <GithubIcon className="fill-light dark:fill-dark  hover:scale-125 transition-all ease duration-200" />
           </a>
-
         </div>
-
-        <div className="w-full  mt-8 md:mt-24 relative font-medium border-t border-solid border-light py-6 px-8 flex  flex-col md:flex-row items-center justify-between">
-          <span className="text-center">
-            &copy;2023 Jared Hooker. All rights reserved.
-          </span>
-          <Link
-            href="/sitemap-0.xml"
-            className="text-center w-full flex justify-center underline my-4 md:my-0"
-          >
-            sitemap.xml
-          </Link>
+        <div className="w-full mt-8 md:mt-24 relative font-medium border-t border-solid border-light py-6 px-8 flex flex-col md:flex-row items-center justify-between">
+          <span className="text-center">&copy;2023 Jared Hooker. All rights reserved.</span>
           <div className="text-center">
             Made by{" "}
             <a href="https://www.hookerhillstudios.com" className="underline" target="_blank">
@@ -84,7 +120,6 @@ const Footer = () => {
         </div>
       </footer>
     </div>
-
   )
 }
 
