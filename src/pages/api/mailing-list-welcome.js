de4ead31c574d02c1e6c5416cdf5fcb33cefe0bf
JRH89 <thegemlins@gmail.com>
2024-06-11 18:16:57 -0700

    k

--- src/pages/api/mailing-list-welcome.js
+++ src/pages/api/mailing-list-welcome.js
@@ -1,21 +1,21 @@
-import sgMail from "@sendgrid/mail"
+import sgMail from "@sendgrid/mail";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    // Retrieve recipient's email address from the request body
-    const { recipient } = req.body;
+    // Retrieve email address from the request body
+    const { email } = req.body;
 
     sgMail.setApiKey("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");
 
     const msg = {
-      to: recipient, // Change to your email address
+      to: email, // Use the email address retrieved from the request body
       from: "hookerhillstudios@gmail.com", // Change to your email address
       subject: "Welcome to my Blog",
       html: `
         <div style="max-width: 600px; margin: 0 auto; text-align: justify; color: black;">
           Thank you for subscribing to my blog. Click the link below if you would like to unsubscribe.
         </div>
-        <p>If you wish to unsubscribe from future emails, please click <a href="https://blog.hookerhillstudios.com?email=${recipient}">here</a>.</p>
+        <p>If you wish to unsubscribe from future emails, please click <a href="https://blog.hookerhillstudios.com?email=${email}">here</a>.</p>
       `,
     };
 
