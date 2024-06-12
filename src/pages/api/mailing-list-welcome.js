127820fad4b7cd30dfd309a029c6f1c2a478dae9
JRH89 <thegemlins@gmail.com>
2024-06-11 17:48:59 -0700

    k

--- src/pages/api/mailing-list-welcome.js
+++ src/pages/api/mailing-list-welcome.js
@@ -1,35 +1,37 @@
 import sgMail from "@sendgrid/mail"
 
 export default async function handler(req, res) {
-	if (req.method === "POST") {
-		
+  if (req.method === "POST") {
+    // Retrieve recipient's email address from the request body
+    const { recipient } = req.body;
 
-		sgMail.setApiKey("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")
+    sgMail.setApiKey("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");
 
-		const msg = {
-			to: "hookerhillstudios@gmail.com",
-			from: "hookerhillstudios@gmail.com",
-			subject: "Welcome to my Blog",
-            html: `<div style="max-width: 600px; margin: 0 auto; text-align: justify; color: black;">
-            Thank you for subscribing to my blog. Click the link below if you would like to unsubscribe.
-          </div>
-     <p>If you wish to unsubscribe from future emails, please click <a href="https://blog.hookerhillstudios.com?email=${recipient}">here</a>.</p>
- </div>`,
-		}
+    const msg = {
+      to: "hookerhillstudios@gmail.com", // Change to your email address
+      from: "hookerhillstudios@gmail.com", // Change to your email address
+      subject: "Welcome to my Blog",
+      html: `
+        <div style="max-width: 600px; margin: 0 auto; text-align: justify; color: black;">
+          Thank you for subscribing to my blog. Click the link below if you would like to unsubscribe.
+        </div>
+        <p>If you wish to unsubscribe from future emails, please click <a href="https://blog.hookerhillstudios.com?email=${recipient}">here</a>.</p>
+      `,
+    };
 
-		try {
-			await sgMail.send(msg)
-			res.status(200).json({ message: "Email sent successfully" })
-		} catch (error) {
-			console.error("Error sending email:", error)
+    try {
+      await sgMail.send(msg);
+      res.status(200).json({ message: "Email sent successfully" });
+    } catch (error) {
+      console.error("Error sending email:", error);
 
-			if (error.response) {
-				console.error("Error response:", error.response.body)
-			}
+      if (error.response) {
+        console.error("Error response:", error.response.body);
+      }
 
-			res.status(500).json({ success: false, error: "Failed to send email" })
-		}
-	} else {
-		res.status(405).json({ message: "Invalid request method" })
-	}
+      res.status(500).json({ success: false, error: "Failed to send email" });
+    }
+  } else {
+    res.status(405).json({ message: "Invalid request method" });
+  }
 }
