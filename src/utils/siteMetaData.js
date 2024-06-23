4e565fc2b1bee3f7dd03afa4e51875f01fd2e344
JRH89 <thegemlins@gmail.com>
2024-06-22 23:49:15 -0700

    desc

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -3,13 +3,13 @@ const siteMetadata = {
   title: "Jared Hooker's Blog",
   author: 'Jared Hooker',
   headerTitle: "Jared Hooker's Blog",
-  description: 'A blog focusing on web development and architectural visualization.',
+  description: "Explore my comprehensive tutorials and discover step-by-step guides to mastering the latest technologies. Whether you're a coding novice or seasoned developer, join me on a journey of learning and creativity in the ever-evolving world of technology.",
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://blog.hookerhillstudios.com', // your website URL
   siteLogo: '/logo.png',
   socialBanner: '/social-banner.png', // add social banner in the public folder
-  email: 'gamedevjared@gmail.com',
+  email: 'jaredroberthooker@gmail.com',
   github: 'https://github.com/jrh89',
   twitter: 'https://twitter.com/hookerhillstudios',
   facebook: 'https://www.facebook.com/jared.hooker.89/',
