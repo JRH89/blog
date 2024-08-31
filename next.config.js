1319f161e46c5d123b8131267ac48ea713e50a39
JRH89 <thegemlins@gmail.com>
2024-08-30 17:35:06 -0700

    redirect

--- next.config.js
+++ next.config.js
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
 
-const {withContentlayer} = require("next-contentlayer")
+const { withContentlayer } = require("next-contentlayer");
 
 const nextConfig = {
-    compiler:{
+    compiler: {
         removeConsole: true,
-    }
+    },
+    async redirects() {
+        return [
+            {
+                source: '/:path*', // Match all paths
+                destination: 'https://www.hookerhillstudios.com/:path*', // Redirect to the same path on the main domain
+                permanent: true, // This makes it a 301 permanent redirect
+            },
+        ];
+    },
 };
 
 module.exports = withContentlayer({ ...nextConfig });
